d32ea0313afb77a6ac290b9dade5c30e27241ea0
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2020-12-14 17:12:33 -0800

    front-end stable enough for backend dev

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,17 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	ListGroupItem
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
 
 const ProductScreen = ({ match }) => {
+	//assigning product ids to Product var or obj haha
 	const product = products.find((p) => p._id === match.params.id);
 	return (
 		<>
 			<Link className='btn btn-light my-3' to='/'>
 				Go Back
 			</Link>
-			<Row></Row>
+
+			<Col md={6}>
+				<Image src={product.image} alt={product.name} fluid />
+			</Col>
+			<Col md={3}>
+				<ListGroup variant='flush'>
+					<ListGroup.Item>
+						<Row>
+							<Col>Price:</Col>
+							<Col>
+								<strong>${product.price}</strong>
+							</Col>
+						</Row>
+					</ListGroup.Item>
+					<ListGroup.Item>
+						<Row>
+							<Col>Status:</Col>
+							<Col>
+								<strong>${product.price}</strong>
+							</Col>
+						</Row>
+					</ListGroup.Item>
+					<ListGroupItem>
+						<Button
+							className='btn-block'
+							type='button'
+							disabled={product.countInStock === 0}>
+							Add to Cart
+						</Button>
+					</ListGroupItem>
+				</ListGroup>
+			</Col>
 		</>
 	);
 };
