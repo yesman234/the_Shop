7b27e275dbd8373d1fcfd7a8ad8ce18091eb37de
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2021-01-11 06:57:11 -0800

    added axios and proxy for PORT

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,10 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import products from "../products";
 
 const HomeScreen = () => {
+	useEffect(() => {});
 	return (
 		<>
 			<h1>Latest Products</h1>
